6a766957660c98892b4811d7734165dff99775cb
Saket Verma <saket3324verma@gmail.com>
2022-03-28 13:06:17 +0530

    Navbar active link complete

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,52 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  @Input() links: string[] = [];
-  constructor() { }
+  @Input() links: any[] = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setActiveLink(event.url.toString());
+      }
+    })
+
+    let array: any[] = [];
+    this.links.forEach(link => {
+      let url = link;
+      let data = { url: url, isActive: false };
+      array.push(data);
+    });
+
+    this.links = array;
+  }
+
+  setActiveLink(url: string) {
+    if (url.includes("gallery")) { this.setActive("gallery") }
+    else if (url.includes("offers")) { this.setActive("offers") }
+    else if (url.includes("feedbacks")) { this.setActive("feedbacks") }
+    else if (url.includes("contact_us")) { this.setActive("contactUs") }
+    else { this.setActive("none") }
+  }
+
+  setActive(active: string) {
+    let arrayString = ["gallery", "offers", "feedbacks", "contactUs"];
+
+    if (active == "none") {
+      this.links.forEach(link => {
+        link.isActive = false;
+      })
+      return;
+    }
+
+    arrayString.forEach((link, index) => {
+      if (link == active) {
+        this.links[index].isActive = true;
+      } else {
+        this.links[index].isActive = false;
+      }
+    })
   }
 
 }

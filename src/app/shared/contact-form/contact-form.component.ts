c783dcd61f7c3ae3a0510f2f36e3fe3f2235b88c
Saket Verma <saket3324verma@gmail.com>
2022-10-23 14:02:44 +0530

    Deployment

--- src/app/shared/contact-form/contact-form.component.ts
+++ src/app/shared/contact-form/contact-form.component.ts
@@ -10,11 +10,11 @@ export class ContactFormComponent implements OnInit {
   isSendingMail: boolean = false;
   isMailSent: boolean = false;
   isSendingTried: boolean = false;
-  public name: any;
-  public email: any;
-  public message: any;
-  public phone: any;
-  public error: string = "";
+  public name: string = "";
+  public email: string = "";
+  public message: string = "";
+  public phone: string = "";
+  public errors: any = { name: false, email: false, phone: false, message: false };
 
   constructor() { }
 
@@ -23,45 +23,84 @@ export class ContactFormComponent implements OnInit {
 
   textInput(type: string, event: any) {
     switch (type) {
-      case "name": this.name = event.target.value; break;
-      case "email": this.email = event.target.value; break;
-      case "phone": this.phone = event.target.value; break;
-      case "message": this.message = event.target.value; break;
+      case "name":
+        if (event?.target?.value !== '') {
+          this.name = event.target.value;
+        } else {
+          this.errors.name = true;
+        }
+        break;
+      case "email":
+        if (event?.target?.value !== '' && ((/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(event.target.value)))) {
+          this.email = event.target.value;
+        } else {
+          this.errors.email = true;
+        }
+        break;
+      case "phone":
+        if (event?.target?.value !== '' && ((/^[0-9]{10}$/).test(event.target.value))) {
+          this.phone = event.target.value;
+        } else {
+          this.errors.phone = true;
+        }
+        break;
+      case "message":
+        if (event?.target?.value !== '') {
+          this.message = event.target.value;
+        } else {
+          this.errors.message = true;
+        }
+        break;
       default: break;
     }
     console.log(event.target.value)
   }
 
   sendMail() {
-    this.isSendingMail = true;
-    this.isMailSent = false;
-
-    let templateParams = {
-      name: this.name,
-      from_email: this.email,
-      to_name: this.email,
-      message: this.message,
-      phone: this.phone
-    }
-
-    emailjs.send(
-      'service_e3hrxr5',
-      'template_cobyj79',
-      templateParams,
-      'BpZ2Og7ymuit3VWlW'
-    ).then((response) => {
-      console.log("response", response);
-      if (response.status == 200) {
-        this.isSendingMail = false;
-        this.isMailSent = true;
-        this.isSendingTried = true;
-      }
-    }, (error) => {
-      console.log("Email Error in service", error);
-      this.isSendingTried = true;
-      this.isSendingMail = false;
+    if (this.name != "" && this.email != "" && this.phone != "" && this.message != "") {
+      this.isSendingMail = true;
       this.isMailSent = false;
-    })
+
+      let templateParams = {
+        name: this.name,
+        from_email: this.email,
+        to_name: this.email,
+        message: this.message,
+        phone: this.phone
+      }
+
+      emailjs.send(
+        'service_e3hrxr5',
+        'template_cobyj79',
+        templateParams,
+        'BpZ2Og7ymuit3VWlW'
+      ).then((response) => {
+        console.log("response", response);
+        if (response.status == 200) {
+          this.isSendingMail = false;
+          this.isMailSent = true;
+          this.isSendingTried = true;
+        }
+      }, (error) => {
+        console.log("Email Error in service", error);
+        this.isSendingTried = true;
+        this.isSendingMail = false;
+        this.isMailSent = false;
+      })
+    } else {
+      if (this.name == "") {
+        this.errors.name = true;
+      }
+      if (this.email == "") {
+        this.errors.email = true;
+      }
+      if (this.phone == "") {
+        this.errors.phone = true;
+      }
+      if (this.message == "") {
+        this.errors.message = true;
+      }
+    }
   }
 
 }
